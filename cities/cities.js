92a768f06340f2dce1cf60da20ade607075386f5
xjiaxiang <xjiaxiang_86@163.com>
2025-02-18 19:41:27 +0800

    feat: 添加模板引擎

--- cities/cities.js
+++ cities/cities.js
@@ -1,10 +1,37 @@
 import {getAreas} from "@home/services/area";
+import Handlebars from "handlebars";
+
+Handlebars.compile("");
+
+const app = document.querySelector("#app");
 
 function run() {
-	getAreas().then((areas) => {
-		// TODO: 渲染出来
-		console.log(areas);
+	const source = document.getElementById("city-item-template")?.innerHTML;
+	if (!source) {
+		console.error(new Error("city-item-template模板不存在"));
+	}
+	const template = Handlebars.compile(source, {});
+	const citiesDomString = template({
+		areas: [
+			{
+				name: "北京",
+			},
+		],
 	});
+
+	console.log(citiesDomString);
+
+	if (app) {
+		app.innerHTML = citiesDomString;
+	} else {
+		console.error(new Error("app元素不存在"));
+	}
+
+	// getAreas().then((areas) => {
+	// 	template({
+	// 		areas,
+	// 	});
+	// });
 }
 
 run();
